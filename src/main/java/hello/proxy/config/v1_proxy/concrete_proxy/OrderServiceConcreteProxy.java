10cf7392bd2768377aa6110dc4ae01beb2e4bd0f
yn15@direa.co.kr <yn15@naver.com>
2024-11-21 17:51:57 +0900

    Create: 구체클래스 기반 프록시 - 적용 extends

---
+++ src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private OrderServiceV2 target;
+    private LogTrace logTrace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        super(null);
+        this.logTrace = logTrace;
+        this.target = target;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try{
+            status = logTrace.begin("OrderService.orderItem()");
+            //target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+
+}
